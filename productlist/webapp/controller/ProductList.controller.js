d0d901b6ac6c5531f037a132f105ec359be99d37
josedsousa <jdesousa@outlook.com>
2023-11-22 14:55:46 +0000

    Products list (w/ filtering)

---
+++ productlist/webapp/controller/ProductList.controller.js
@@ -0,0 +1,111 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, Filter, FilterOperator, JSONModel) {
+    "use strict";
+
+    return Controller.extend(
+      "productslist.productlist.controller.ProductList",
+      {
+        onInit: function () {
+          this.oFilterBar = this.getView().byId("filterBar");
+          this.oTable = this.getView().byId("table");
+
+          var that = this,
+            oModel = new JSONModel();
+
+          // var obj = oModel.getObject("/Products(0)");
+          // Model not loaded at this point, therefore method getObject() will not return data
+          this.getRatings(this.getOwnerComponent().getModel()).then(function (
+            value
+          ) {
+            oModel.setData(value);
+            that.getView().setModel(oModel, "ratingsModel");
+          });
+        },
+
+        getRatings: function (oModel) {
+          return new Promise((resolve) => {
+            oModel.read("/Products", {
+              success: function (oResponse) {
+                var ratings = new Set([]),
+                  ratingsJSON = { ratings: [] };
+
+                oResponse.results.map(function (value) {
+                  //A set is used in order to remove duplicates
+                  ratings.add(value.Rating);
+                });
+
+                ratings.forEach(function (value) {
+                  ratingsJSON.ratings.push({ value: value });
+                });
+
+                //Sort in descending order
+                ratingsJSON.ratings.sort(function (a, b) {
+                  return a.value - b.value;
+                });
+
+                resolve(ratingsJSON);
+              },
+            });
+          });
+        },
+
+        onSearch: function () {
+          var aTableFilters = this.oFilterBar
+            .getFilterGroupItems()
+            .reduce(function (aResult, oFilterGroupItem) {
+              var oControl = oFilterGroupItem.getControl(),
+                controlType = oControl.getMetadata().getName();
+
+              switch (controlType) {
+                case "sap.m.DatePicker":
+                  var oBeginDate = oControl.getDateValue();
+
+                  if (oBeginDate) {
+                    var oEndDate = new Date(oBeginDate);
+                    oEndDate.setHours(23, 59, 59);
+
+                    aResult.push(
+                      new Filter({
+                        path: oFilterGroupItem.getName(),
+                        operator: FilterOperator.BT,
+                        value1: oBeginDate,
+                        value2: oEndDate,
+                      })
+                    );
+                  }
+                  break;
+
+                default:
+                  var inputValue = oControl.getValue();
+
+                  if (inputValue) {
+                    aResult.push(
+                      new Filter({
+                        path: oFilterGroupItem.getName(),
+                        operator: FilterOperator.EQ,
+                        value1: inputValue,
+                      })
+                    );
+                  }
+                  break;
+              }
+
+              return aResult;
+            }, []);
+
+          this.oTable.getBinding("items").filter(aTableFilters);
+          this.oTable.setShowOverlay(false);
+        },
+      }
+    );
+  }
+);
---
+++ productlist/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"productslist/productlist/test/unit/controller/ProductList.controller"
+], function () {
+	"use strict";
+});
